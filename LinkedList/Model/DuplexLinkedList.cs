afad3277296790833d7202a62f3d27eff9da65ca
Илья <ilel@list.ru>
2019-07-03 09:42:50 +0700

    Добавлены двусвязный и кольцевой списки.

---
+++ LinkedList/Model/DuplexLinkedList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedList.Model
+{
+    public class DuplexLinkedList<T> : IEnumerable<T>
+    {
+        public DuplexItem<T> Head { get; set; }
+        public DuplexItem<T> Tail { get; set; }
+        public int Count { get; set; }
+
+        public DuplexLinkedList(T data)
+        {
+            var item = new DuplexItem<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public DuplexLinkedList() { }
+
+        public void Add(T data)
+        {
+            var item = new DuplexItem<T>(data);
+
+            if (Count == 0)
+            {
+                Head = item;
+                Tail = item;
+                Count = 1;
+                return;
+            }
+
+            Tail.Next = item;
+            item.Previos = Tail;
+            Tail = item;
+            Count++;
+        }
+
+        public void Delete(T data)
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    current.Previos.Next = current.Next;
+                    current.Next.Previos = current.Previos;
+                    Count--;
+                    return;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+        }
+
+        public DuplexLinkedList<T> Reverse()
+        {
+            var result = new DuplexLinkedList<T>();
+
+            var current = Tail;
+
+            while (current != null)
+            {
+                result.Add(current.Data);
+                current = current.Previos;
+            }
+
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                yield return current;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumerator();
+        }
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinkedList.Model;
+using System;
 
 namespace LinkedList
 {
@@ -6,6 +7,63 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CircularLinkedList:");
+
+            var circularList = new CircularLinkedList<int>();
+            circularList.Add(1);
+            circularList.Add(2);
+            circularList.Add(3);
+            circularList.Add(4);
+            circularList.Add(5);
+
+            foreach (var item in circularList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\nCircularLinkedList -> Delete");
+
+            circularList.Delete(3);
+
+            foreach (var item in circularList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\n\nDuplexLinkedList:");
+
+            var duplexList = new DuplexLinkedList<int>();
+            duplexList.Add(1);
+            duplexList.Add(2);
+            duplexList.Add(3);
+            duplexList.Add(4);
+            duplexList.Add(5);
+
+            foreach (var item in duplexList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\nDuplexLinkedList -> Delete:");
+
+            duplexList.Delete(3);
+
+            foreach (var item in duplexList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\nDuplexLinkedList -> Reverse:");
+
+            var reverseDuplexList = duplexList.Reverse();
+
+            foreach (var item in reverseDuplexList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine("\n\nLinkedList:");
+
             var list = new Model.LinkedList<int>();
 
             list.Add(1);
@@ -16,10 +74,10 @@ namespace LinkedList
 
             foreach (var item in list)
             {
-                Console.Write(item + " ");
+                Console.Write($"{item} ");
             }
 
-            Console.WriteLine();
+            Console.WriteLine("\nLinkedList -> Delete:");
 
             list.Delelete(3);
             list.Delelete(1);
@@ -27,25 +85,25 @@ namespace LinkedList
 
             foreach (var item in list)
             {
-                Console.Write(item + " ");
+                Console.Write($"{item} ");
             }
 
-            Console.WriteLine();
+            Console.WriteLine("\nLinkedList -> AppendHead:");
 
             list.AppendHead(7);
 
             foreach (var item in list)
             {
-                Console.Write(item + " ");
+                Console.Write($"{item} ");
             }
 
-            Console.WriteLine();
+            Console.WriteLine("\nLinkedList -> InsertAfter:");
 
             list.InsertAfter(4, 8);
 
             foreach (var item in list)
             {
-                Console.Write(item + " ");
+                Console.Write($"{item} ");
             }
 
             Console.ReadLine();
